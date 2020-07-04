6018a15e594665869769dfff184dcf6f94e835df
pluettmann <pluettmann@uni-osnabrueck.de>
2020-07-04 16:12:18 +0200

    added keywords

---
+++ Token/Keyword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cahmann.Token {
+    /// <summary>
+    /// represents a collection of keywords that are interpreted into C# code
+    /// </summary>
+    public class Keyword {
+
+        public Keyword() {
+        }
+    }
+}
