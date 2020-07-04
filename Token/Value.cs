b7e1f4765572a7404e196a2e14d9ad7576645ecd
pluettmann <pluettmann@uni-osnabrueck.de>
2020-07-04 16:12:32 +0200

    added values

---
+++ Token/Value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CahmannInterpreter.Token {
+    /// <summary>
+    /// represents any form of values.
+    /// holds data types.
+    /// </summary>
+    public class Value {
+
+        public Value() {
+        }
+    }
+
+}
