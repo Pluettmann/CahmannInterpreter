ae7b984c8e584b8671aea0dcea28e92910b89523
Simon Cahill <simon@h3lix.de>
2020-07-11 20:57:30 +0200

    Added new Extensions file

---
+++ src/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cahmann.Interpreter {
+
+    using System.IO;
+
+    /// <summary>
+    /// Contains extension methods used throughout the interpreter.
+    /// </summary>
+    public static class Extensions {
+
+        /// <summary>
+        /// Reads all the text from a <see cref="FileInfo"/> object.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>The entire contents of a file as a string</returns>
+        public static string ReadAllText(this FileInfo file) => File.ReadAllText(file?.FullName);
+
+    }
+}
