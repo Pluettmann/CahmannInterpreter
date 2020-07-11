a3da43d58ce59aa3200deca7fc59ec6d5e2b2cf3
Simon Cahill <simon@h3lix.de>
2020-07-11 13:11:30 +0200

    Began implementing Cahmann Lexer.

---
+++ src/Token/IToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cahmann.Interpreter.Token {
+
+    /// <summary>
+    /// Base Token type.
+    /// </summary>
+    public interface IToken {
+
+        /// <summary>
+        /// Gets or sets the token type.
+        /// </summary>
+        Enum TokenType { get; set; }
+
+    }
+}
---
+++ src/Token/SpecialTokenType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cahmann.Interpreter.Token {
+
+    /// <summary>
+    /// Contains special token types.
+    /// </summary>
+    public enum  SpecialTokenType {
+
+        /// <summary>
+        /// Marks the end of a Cahmann Script file.
+        /// </summary>
+        EndOfFile,
+
+    }
+}
