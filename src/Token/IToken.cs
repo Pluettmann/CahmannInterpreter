a4785df70eb6f79d71a9baab05d3acdd1b3b95e7
Simon Cahill <simon@h3lix.de>
2020-07-11 21:00:58 +0200

    Added new IToken interface from which all Token classes inherit

--- src/Token/IToken.cs
+++ src/Token/IToken.cs
@@ -2,6 +2,8 @@
 
 namespace Cahmann.Interpreter.Token {
 
+    using System.IO;
+
     /// <summary>
     /// Base Token type.
     /// </summary>
@@ -11,6 +13,26 @@ namespace Cahmann.Interpreter.Token {
         /// Gets or sets the token type.
         /// </summary>
         Enum TokenType { get; set; }
+
+        /// <summary>
+        /// A string representing the token.
+        /// </summary>
+        string StringRepresentation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file in which the code is located.
+        /// </summary>
+        FileInfo SourceFile { get; set; }
+
+        /// <summary>
+        /// Gets the line number in the file 
+        /// </summary>
+        int Line { get; set; }
+
+        /// <summary>
+        /// Gets the position in <see cref="Line"/> in <see cref="SourceFile"/>
+        /// </summary>
+        int PositionInLine { get; set; }
 
     }
 }
